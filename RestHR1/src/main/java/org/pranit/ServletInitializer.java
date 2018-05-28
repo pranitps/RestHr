323ed80528404a2f8c8541ae2ee548f03612697e
pranitps <pranitps@192.168.0.14>
2018-05-28 19:58:19 -0400

    Second commit

--- RestHR1/src/main/java/org/pranit/ServletInitializer.java
+++ RestHR1/src/main/java/org/pranit/ServletInitializer.java
@@ -5,6 +5,9 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
+	/**
+	 * This is seccond commit.
+	 */
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(RestHr1Application.class);
